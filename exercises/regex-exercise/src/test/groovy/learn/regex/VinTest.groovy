4ae6c7aab1d1fb7ba7ab3f99bca22133d270ec9e
jmccann-dev-10 <jmccann@dev-10.com>
2023-02-08 12:07:27 -0600

    added vin tests

---
+++ exercises/regex-exercise/src/test/groovy/learn/regex/VinTest.groovy
@@ -0,0 +1,59 @@
+package learn.regex
+
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.assertEquals
+
+class VinTest {
+
+    static RegexTester regexTester;
+
+    @BeforeAll
+    static void init() throws IOException {
+        regexTester = new RegexTester("./data/vins");
+    }
+
+    // HINT: This will probably be useful: https://driving-tests.org/vin-decoder/
+
+    @Test
+    void findValidVins() {
+        String regex = /REGEX HERE/
+        List<String> validVins = regexTester.findAll(regex)
+
+        assertEquals(499, validVins.size())
+    }
+
+    @Test
+    void findInvalidVins() {
+        String regex = /REGEX HERE/
+        List<String> invalidVins = regexTester.findAll(regex)
+
+        assertEquals(35, invalidVins.size())
+    }
+
+    @Test
+    void findValidVinsMadeInJapan() {
+        String regex = /REGEX HERE/
+        List<String> vehicleVinsMadeInJapan = regexTester.findAll(regex)
+
+        assertEquals(56, vehicleVinsMadeInJapan.size())
+    }
+
+    @Test
+    void findValidVinsMadeIn2002() {
+        String regex = /REGEX HERE/
+        List<String> vinsMadeIn2002 = regexTester.findAll(regex)
+
+        assertEquals(11, vinsMadeIn2002.size())
+    }
+
+    // HINT: You may want to use this: https://en.wikibooks.org/wiki/Vehicle_Identification_Numbers_(VIN_codes)/Honda/VIN_Codes#Americas
+
+    @Test
+    void findCanadianMadeHondaVinsMadeFrom2003To2013_CHALLENGE() {
+        String regex = /REGEX HERE/
+        List<String> canadianMadeHondasFrom2010To2013 = regexTester.findAll(regex)
+        assertEquals(5, canadianMadeHondasFrom2010To2013.size())
+    }
+}
